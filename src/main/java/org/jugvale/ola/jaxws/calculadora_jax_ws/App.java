09748029af4ae447543168335cdda7a6ca53bcf2
achaitanya <achaitanya@ucsd.edu>
2019-03-22 10:46:22 -0700

    first commit

---
+++ src/main/java/org/jugvale/ola/jaxws/calculadora_jax_ws/App.java
@@ -0,0 +1,13 @@
+package org.jugvale.ola.jaxws.calculadora_jax_ws;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
